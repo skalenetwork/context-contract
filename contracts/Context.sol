c0a4f5d020eaf911338dcb1e334bfe8e2560019b
DmytroNazarenko <dmytro.nazarenko@nure.ua>
2022-06-16 20:05:59 +0300

    Add Context contract

---
+++ contracts/Context.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.5.8;
+
+contract Context
+{
+    address owner;
+    string schainName = "SKALE";
+
+    function getSchainName() public view returns (string memory)
+    {
+        return schainName;
+    }
+    function getSchainOwnerAddress() public view returns (address)
+    {
+        return owner;
+    }
+    function setSchainOwnerAddress(address newOwner) public
+    {
+        require(msg.sender == owner);
+        owner = newOwner;
+    }
+}
